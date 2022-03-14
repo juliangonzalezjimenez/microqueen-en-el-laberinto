33f3fccf2be613d8884e731916bd68fc6476cac2
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-03-14 11:49:39 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+function girar90grados () {
+    maqueen.motorStop(maqueen.Motors.All)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+    basic.pause(260)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+}
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        girar90grados()
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    }
+})

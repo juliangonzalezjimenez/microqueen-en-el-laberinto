8ed6f404343a5a6b6f5ce29b3763f00a3db9f264
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-03-16 08:59:48 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-function girar90grados () {
+function girar90gradosaderecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(260)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    basic.pause(500)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+}
+function girar90gradosizquierda () {
     maqueen.motorStop(maqueen.Motors.All)
     basic.pause(260)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
@@ -6,10 +14,11 @@ function girar90grados () {
     basic.pause(500)
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 }
+let gira90grados = 0
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
         maqueen.motorStop(maqueen.Motors.All)
-        girar90grados()
+        girar90gradosizquierda()
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
